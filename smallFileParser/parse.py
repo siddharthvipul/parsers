9e8f3a7eef181880e2332347a14a994861ef985a
siddharthvipul <siddharthvipul1@gmail.com>
2018-06-04 15:32:48 +0530

    rm .swp

--- smallFileParser/parse.py
+++ smallFileParser/parse.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 '''
-doc-string holder
+This program is for smallFile, parses the output and puts important numbers in a
+csv file.
+
+Use "$ ./parse.py -h" to see the options
+
 '''
-
-
 import re
 import argparse
 import csv
 import os.path
-
 
 def shred_digit(line):
     '''
     This function fetches numbers from a line
     '''
     return re.findall(r'\d+.\d+', line)
-
 
 def find_pattern(file_name):
     '''
@@ -53,6 +53,7 @@ def main():
     input_help = 'smallfile output file name'
     output_help = 'export parsed result in a file (csv) of name ..'
 
+    header = ['Total Data', 'Files/sec', 'IOPS', 'MiB/sec']
     parser = argparse.ArgumentParser(description='SmallFile Parser')
     parser.add_argument('-i', '--input', type=str, help=input_help)
     parser.add_argument('-o', '--output', type=str, help=output_help)
@@ -65,14 +66,12 @@ def main():
             if os.path.isfile(args.output+".csv"):
                 write_to_csv(result, args.output)
             else:
-                write_to_csv(['Total Data', 'Files/sec', 'IOPS', 'MiB/sec'], args.output)
+                write_to_csv(header , args.output)
                 write_to_csv(result, args.output)
         else:
             print(result)
     else:
         parser.print_help()
-
-
 
 if __name__ == '__main__':
     main()
